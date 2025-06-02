486fc2d48eaa080d59a898203d0d5b3b92ea7bd4
Sami Jaghouar <sami.jaghouar@hotmail.fr>
2025-06-02 16:29:51 +0000

    add reward to the log

--- src/zeroband/infer.py
+++ src/zeroband/infer.py
@@ -306,12 +306,10 @@ def inference(config: Config):
         request_rewards = compute_vllm_rewards(request_outputs, verification_infos, task_types, config.rewards)
         logger.info(f"Computed rewards and advantages in {time.time() - start:.2f}s")
 
-        total_rewards = sum(sum(r.reward for r in req.rewards) for req in request_rewards)
-        total_completions = sum(len(req.rewards) for req in request_rewards)
-        avg_reward_to_log = total_rewards / total_completions
+        batch_rewards = sum(sum(r.reward for r in req.rewards) for req in request_rewards) / batch_samples
 
-        monitor.log({"avg_reward": avg_reward_to_log})
-        logger.info(f"Average reward of the batch: {avg_reward_to_log}")
+        monitor.log({"rewards/batch_rewards": batch_rewards})
+        logger.info(f"Average reward of the batch: {batch_rewards}")
 
         # Get parquet table
         table = get_parquet_table(
