5424009c12124f7fbe606438d83100eb1466950f
Mika Senghaas <mail@mikasenghaas.de>
2025-07-02 04:04:28 +0000

    Move deps

--- install.sh
+++ install.sh
@@ -44,7 +44,7 @@ main() {
     fi
 
     log_info "Installing dependencies in virtual environment..."
-    uv sync && uv sync --all-extras
+    uv sync && uv sync --extra fa
     log_info "Installation completed!"
 }
 
