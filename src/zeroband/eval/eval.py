1eacc796ef93e72b01374f128d6779f67a7189cd
sami jaghouar <sami.jaghouar@gmail.com>
2025-07-07 15:49:47 +0000

    fix unit test config

--- src/zeroband/eval/eval.py
+++ src/zeroband/eval/eval.py
@@ -5,12 +5,10 @@ from zeroband.eval.utils import run_benchmark
 from zeroband.training.orchestrator.client import (
     check_has_model,
     check_health,
-    reload_weights,
     reset_weights,
     setup_client,
 )
 from zeroband.training.orchestrator.logger import setup_logger
-from zeroband.training.orchestrator.utils import wait_for_weight_checkpoint
 from zeroband.utils.monitor import setup_monitor
 from zeroband.utils.pydantic_config import parse_argv
 from zeroband.utils.utils import clean_exit
@@ -23,7 +21,7 @@ async def eval(config: EvalConfig):
     logger.info("Starting evaluation")
     logger.info(f"Model: {config.model}")
     logger.info(f"Sampling: {config.sampling}")
-    logger.info(f"Evaluation: {config.eval}")
+    logger.info(f"Benchmarks: {config.benchmarks}")
 
     # Initialize the monitor
     logger.info(f"Initializing monitor ({config.monitor})")
@@ -45,7 +43,7 @@ async def eval(config: EvalConfig):
 
     # Run benchmarks on base model
     logger.info(f"Running evals on base model {config.model.name}")
-    for benchmark in config.eval.benchmarks:
+    for benchmark in config.benchmarks:
         await run_benchmark(
             client,
             benchmark,
@@ -54,38 +52,6 @@ async def eval(config: EvalConfig):
             step=0,
             use_tqdm=config.use_tqdm,
         )
-
-    # If specified, run online evaluation
-    if config.eval.online:
-        logger.info(
-            f"Running online evaluation on {config.model.name} every {config.eval.online.interval} steps from checkpoint directory {config.eval.online.ckpt_path}"
-        )
-        step = config.eval.online.interval
-        while True:
-            # Wait for checkpoint to be available
-            wait_for_weight_checkpoint(config.eval.online.ckpt_path, step)
-            await reload_weights(client, config.eval.online.ckpt_path, step)
-
-            # Run benchmarks on new checkpoint
-            logger.info(f"Running evals for checkpoint step {step}")
-            for benchmark in config.eval.benchmarks:
-                await run_benchmark(
-                    client,
-                    benchmark,
-                    config.model,
-                    config.sampling,
-                    step,
-                    use_tqdm=config.use_tqdm,
-                )
-
-            # Update eval step to next checkpoint step
-            step += config.eval.online.interval
-
-            if config.eval.online.max_steps and step > config.eval.online.max_steps:
-                logger.info(
-                    f"Reached maximum number of steps ({config.eval.online.max_steps}). Stopping online evaluation."
-                )
-                break
 
     logger.info("Evaluation finished!")
 
