3eb0eee3666db7903f0c9eac76f74a9f3b1f77f4
William Brown <williambrown97@gmail.com>
2025-07-09 10:54:26 +0000

    vf simple-math

--- src/zeroband/environments/registry.py
+++ src/zeroband/environments/registry.py
@@ -41,10 +41,57 @@ Provide the final numerical answer inside \\boxed{{...}}."""
     return vf_env
 
 
+def load_simple_math_environment(env_args: dict = {}) -> Environment:
+    import json
+
+    def get_valid_answers(x: dict) -> list[str]:
+        one_or_more_answers = json.loads(x["verification_info"])["ground_truth"]
+        if isinstance(one_or_more_answers, str):
+            return [one_or_more_answers]
+        elif isinstance(one_or_more_answers, list):
+            return one_or_more_answers
+        else:
+            raise ValueError(f"Invalid answer format: {one_or_more_answers}")
+
+    train_dataset = load_dataset("justus27/math-hendrycks-genesys-format", split="train").map(
+        lambda x: {"question": x["prompt"], "info": {"answers": get_valid_answers(x)}, "task": "simple-math"}
+    )
+    train_dataset = train_dataset.remove_columns(["prompt", "verification_info"])
+
+    from verifiers.utils.data_utils import extract_boxed_answer
+
+    parser = vf.ThinkParser()
+    from src.zeroband.training.orchestrator.genesys.math_utils import grade_answer_mathd, grade_answer_sympy
+
+    def grade_answer(response: str, answers: list[str]) -> float:
+        for answer in answers:
+            if "\\boxed{" in answer:
+                answer = extract_boxed_answer(answer)
+            if grade_answer_mathd(response, answer) or grade_answer_sympy(response, answer):
+                return 1.0
+        return 0.0
+
+    def correct_answer_reward_func(completion, info, **kwargs) -> float:
+        response = parser.parse_answer(completion) or ""
+        if "\\boxed{" in response:
+            response = extract_boxed_answer(response)
+        is_correct = [grade_answer(response, answer) for answer in info["answers"]]
+        return max(is_correct)
+
+    rubric = vf.Rubric(
+        funcs=[
+            correct_answer_reward_func,
+        ],
+        weights=[1.0],
+    )
+    vf_env = vf.SingleTurnEnv(dataset=train_dataset, parser=parser, rubric=rubric)
+    return vf_env
+
+
 def load_reverse_environment(env_args: dict = {}) -> Environment:
     import json
-    import re
 
+    # import re
     # train_dataset = load_dataset("agentlans/wikipedia-paragraphs", split="train").map(
     #    lambda x: {
     #        "question": "Reverse the text in quotation marks character-by-character: "
@@ -80,10 +127,7 @@ def load_reverse_environment(env_args: dict = {}) -> Environment:
 
             return SequenceMatcher(None, x, y).ratio()
 
-        answer_text = re.search(r"<answer>(.*?)</answer>", completion[-1]["content"], re.DOTALL)
-        if not answer_text:
-            return 0
-        response = answer_text.group(1).strip()
+        response = parser.parse_answer(completion) or ""
         return lcs_ratio(response, answer)
 
     rubric = vf.Rubric(
