fix logprob

src/prime_rl/trainer/loss.py
@@ -7,7 +7,6 @@ from torch import Tensor
 from torch.nn import functional as F
 
 from prime_rl.trainer.config import LossConfig
-from prime_rl.trainer.model import Model, forward
 
 
 @dataclass
@@ -21,46 +20,38 @@ class RatioInfo:
 
 @jaxtyped(typechecker=typechecker)
 def grpo_loss(
-    shifted_logits: Float[Tensor, "batch seq vocab"],
-    input_ids: Int[Tensor, "batch seq"],
+    logprobs: Float[Tensor, "batch seq"],
     advantages: Float[Tensor, "batch seq"],
     original_logprobs: Float[Tensor, "batch seq"],
     loss_mask: Int[Tensor, "batch seq"],
-    temperature: float,
     loss_config: LossConfig,
 ) -> tuple[Tensor, RatioInfo]:
     if loss_config.type == "clip":
         return grpo_loss_clip(
-            shifted_logits=shifted_logits,
-            input_ids=input_ids,
+            logprobs=logprobs,
             advantages=advantages,
             original_logprobs=original_logprobs,
             loss_mask=loss_mask,
-            temperature=temperature,
             epsilon_low=loss_config.epsilon_low,
             epsilon_high=loss_config.epsilon_high,
             clip_ratio=loss_config.clip_ratio,
         )
     elif loss_config.type == "ratio":
         return grpo_loss_ratio(
-            shifted_logits=shifted_logits,
-            input_ids=input_ids,
+            logprobs=logprobs,
             advantages=advantages,
             original_logprobs=original_logprobs,
             loss_mask=loss_mask,
-            temperature=temperature,
             clip_ratio=loss_config.clip_ratio,
         )
 
 
 @jaxtyped(typechecker=typechecker)
 def grpo_loss_clip(
-    shifted_logits: Float[Tensor, "batch seq vocab"],
-    input_ids: Int[Tensor, "batch seq"],
+    logprobs: Float[Tensor, "batch seq"],
     advantages: Float[Tensor, "batch seq"],
     original_logprobs: Float[Tensor, "batch seq"],
     loss_mask: Int[Tensor, "batch seq"],
-    temperature: float,
     epsilon_low: float,
     epsilon_high: float,
     clip_ratio: float,
@@ -76,12 +67,8 @@ def grpo_loss_clip(
         epsilon: Clipping parameter for PPO
         ignore_index: Specifies a target value that is ignored and does not contribute to the loss
     """
-    # Divide logits by sampling temperature.
-    # See https://huggingface.co/blog/the_n_implementation_details_of_rlhf_with_ppo#policy-training-implementation-details
-    shifted_logits = shifted_logits / temperature
-    per_token_logps = selective_log_softmax(shifted_logits, input_ids)
 
-    raw_ratio = torch.exp(per_token_logps - original_logprobs)
+    raw_ratio = torch.exp(logprobs - original_logprobs)
 
     coef_1 = torch.clamp(raw_ratio, 0, clip_ratio)
 
@@ -108,20 +95,13 @@ def grpo_loss_clip(
 
 @jaxtyped(typechecker=typechecker)
 def grpo_loss_ratio(
-    shifted_logits: Float[Tensor, "batch seq vocab"],
-    input_ids: Int[Tensor, "batch seq"],
+    logprobs: Float[Tensor, "batch seq"],
     advantages: Float[Tensor, "batch seq"],
     original_logprobs: Float[Tensor, "batch seq"],
     loss_mask: Int[Tensor, "batch seq"],
-    temperature: float,
     clip_ratio: float,
 ) -> tuple[Tensor, RatioInfo]:
-    # Divide logits by sampling temperature.
-    # See https://huggingface.co/blog/the_n_implementation_details_of_rlhf_with_ppo#policy-training-implementation-details
-    shifted_logits = shifted_logits / temperature
-    per_token_logps = selective_log_softmax(shifted_logits, input_ids)
-
-    raw_ratio = torch.exp(per_token_logps - original_logprobs)
+    raw_ratio = torch.exp(logprobs - original_logprobs)
 
     is_clipped = (raw_ratio > clip_ratio).float()
     clipped_token_count = _masked_sum(is_clipped, loss_mask)
@@ -184,17 +164,15 @@ def selective_log_softmax(
 
 @jaxtyped(typechecker=typechecker)
 def compute_logprobs(
-    model: Model,
+    logits: Float[Tensor, "batch seq vocab"],
     input_ids: Int[Tensor, "batch seq"],
-    position_ids: Int[Tensor, "batch seq"],
     temperature: float,
-) -> Float[Tensor, "batch seq"]:
-    logits = forward(model, input_ids, position_ids).contiguous()
+) -> tuple[Float[Tensor, "batch seq"], Float[Tensor, "batch seq vocab"]]:
     shifted_logits = shift_logits(logits)
     shifted_logits = shifted_logits / temperature
     logprobs = selective_log_softmax(shifted_logits, input_ids)
-    del logits, shifted_logits
-    return logprobs
+    del logits
+    return logprobs, shifted_logits
 
 
 @jaxtyped(typechecker=typechecker)

src/prime_rl/trainer/train.py
@@ -19,7 +19,7 @@ from prime_rl.trainer.weights import WeightCheckpointManager
 from prime_rl.trainer.config import TrainerConfig
 from prime_rl.trainer.data import DataLoader, FakeDataLoader
 from prime_rl.trainer.logger import setup_logger
-from prime_rl.trainer.loss import grpo_loss, compute_entropy, shift_logits, compute_logprobs
+from prime_rl.trainer.loss import grpo_loss, compute_entropy, compute_logprobs
 from prime_rl.trainer.model import (
     forward,
     get_tokenizer,
@@ -199,11 +199,13 @@ def train(config: TrainerConfig):
                     logprobs = micro_batch["logprobs"].to("cuda")
                     temperature = micro_batch["temperature"]
 
-                    recomputed_logprobs = compute_logprobs(logprob_model, input_ids, position_ids, temperature)
+                    logits = forward(logprob_model, input_ids, position_ids).contiguous()
+                    recomputed_logprobs, _ = compute_logprobs(logits, input_ids, temperature)
+
                     recomputed_logprob_error = (torch.exp(recomputed_logprobs - logprobs) * loss_mask).sum()
 
                     micro_batch["recomputed_logprob_error"] = recomputed_logprob_error.to("cpu")
-                    micro_batch["logprobs"] = recomputed_logprobs.to("cpu")
+                    micro_batch["og_logprobs"] = recomputed_logprobs.to("cpu")
 
             # here we sepcifically don't save the tensor offloaded, they are alreay consumed and we will never use it again.
             # this avoid having to make sure we don't keep too much tensor offloaded in cpu memory
@@ -232,23 +234,20 @@ def train(config: TrainerConfig):
             position_ids = micro_batch["position_ids"].to("cuda")
             advantages = micro_batch["advantages"].to("cuda")
             loss_mask = micro_batch["loss_mask"].to("cuda")
-            logprobs = micro_batch["logprobs"].to("cuda")
+            og_logprobs = micro_batch["og_logprobs"].to("cuda")
             temperature = micro_batch["temperature"]
             micro_batch_size, seq_len = input_ids.shape
 
             # Forward pass
             logits = forward(model, input_ids, position_ids).contiguous()
-            shifted_logits = shift_logits(logits)
-            del logits
+            logprobs, shifted_logits = compute_logprobs(logits, input_ids, temperature)
 
             # Compute loss
             loss, ratio_info = grpo_loss(
-                shifted_logits=shifted_logits,
-                input_ids=input_ids,
+                logprobs=logprobs,
                 advantages=advantages,
-                original_logprobs=logprobs,
+                original_logprobs=og_logprobs,
                 loss_mask=loss_mask,
-                temperature=temperature,
                 loss_config=config.loss,
             )
 
@@ -270,7 +269,7 @@ def train(config: TrainerConfig):
             loss_metrics["loss/raw_importance_ratio"] += ratio_info.raw_ratio_sum.detach().float()
             max_ratio = max(max_ratio, ratio_info.raw_ratio_max.detach().float())
 
-            recomputed_logprob_error: Tensor = micro_batch.get("recomputed_logprob_error", torch.tensor(0.0))
+            recomputed_logprob_error: Tensor = micro_batch.get("recomputed_logprob_error", torch.tensor(1.0))
             loss_metrics["loss/recomputed_logprob_error"] += recomputed_logprob_error.detach().float()
 
             # Scale loss by scale factor before backward pass