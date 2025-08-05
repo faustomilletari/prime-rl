81b2b2a80d4efcb80be06123675dea218abfa1f0
Mika Senghaas <mail@mikasenghaas.de>
2025-08-05 20:58:58 +0000

    Also log sum of prompt/completion/seq len

--- src/prime_rl/orchestrator/orchestrator.py
+++ src/prime_rl/orchestrator/orchestrator.py
@@ -347,6 +347,7 @@ async def orchestrate(config: OrchestratorConfig):
             "seq_len/max": seq_lens.max().item(),
             "seq_len/min": seq_lens.min().item(),
             "seq_len/std": seq_lens.std().item(),
+            "seq_len/sum": seq_lens.sum().item(),
             "step": progress.step,
         }
         monitor.log(seq_len_metrics)
@@ -356,6 +357,7 @@ async def orchestrate(config: OrchestratorConfig):
             "prompt_len/max": prompt_lens.max().item(),
             "prompt_len/min": prompt_lens.min().item(),
             "prompt_len/std": prompt_lens.std().item(),
+            "prompt_len/sum": prompt_lens.sum().item(),
             "step": progress.step,
         }
         monitor.log(prompt_len_metrics)
@@ -365,6 +367,7 @@ async def orchestrate(config: OrchestratorConfig):
             "completion_len/max": completion_lens.max().item(),
             "completion_len/min": completion_lens.min().item(),
             "completion_len/std": completion_lens.std().item(),
+            "completion_len/sum": completion_lens.sum().item(),
             "step": progress.step,
         }
         monitor.log(completion_len_metrics)
