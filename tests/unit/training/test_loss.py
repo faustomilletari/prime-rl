df54edd1a8a3a55fb500618247681d40f4c5b46a
Fares Obeid <fares.obeid24@imperial.ac.uk>
2025-08-21 22:11:04 +0000

    fix tests

--- tests/unit/training/test_loss.py
+++ tests/unit/training/test_loss.py
@@ -1,7 +1,8 @@
 import pytest
 import torch
 
-from prime_rl.trainer.rl.loss import compute_entropy, grpo_loss_clip, grpo_loss_ratio
+from prime_rl.trainer.rl.config import LossConfig
+from prime_rl.trainer.rl.loss import compute_entropy, compute_loss
 
 pytestmark = [pytest.mark.gpu]
 
@@ -11,27 +12,31 @@ def test_grpo_loss():
     old_logprobs = torch.randn(100, dtype=torch.float32).cuda()
     advantages = torch.randn(100).cuda()
 
-    loss, _ = grpo_loss_clip(
+    loss, _ = compute_loss(
         logprobs,
         old_logprobs,
         advantages,
-        epsilon_low=0.2,
-        epsilon_high=0.2,
-        clip_ratio=10.0,
+        loss_mask=torch.ones(100, dtype=torch.bool).cuda(),
+        position_ids=torch.arange(100, dtype=torch.int32).cuda(),
+        loss_config=LossConfig(type="grpo", clip_ratio_low=0.0, clip_ratio_high=10.0),
+        loss_scale=1.0,
     )
     assert loss.shape == (100,)
 
 
-def test_grpo_loss_ratio():
+def test_gspo_loss():
     logprobs = torch.randn(100, dtype=torch.float32).cuda()
     old_logprobs = torch.randn(100, dtype=torch.float32).cuda()
     advantages = torch.randn(100).cuda()
 
-    loss, _ = grpo_loss_ratio(
+    loss, _ = compute_loss(
         logprobs,
         old_logprobs,
         advantages,
-        clip_ratio=10.0,
+        loss_mask=torch.ones(100, dtype=torch.bool).cuda(),
+        position_ids=torch.arange(100, dtype=torch.int32).cuda(),
+        loss_config=LossConfig(type="gspo", clip_ratio_low=0.0, clip_ratio_high=10.0),
+        loss_scale=1.0,
     )
     assert loss.shape == (100,)
 
