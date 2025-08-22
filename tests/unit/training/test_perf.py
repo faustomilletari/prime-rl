22fabc8f7e24cfe447c8203b11aa7d634736932c
Jackmin801 <ongjackm@gmail.com>
2025-08-22 00:19:32 +0000

    remove comment

--- tests/unit/training/test_perf.py
+++ tests/unit/training/test_perf.py
@@ -13,7 +13,6 @@ torch.set_default_device("meta")
     [("Qwen/Qwen3-0.6B", 595_984_384, 3_928_227_840), ("Jackmin108/debug-moe-0.5B", 256_442_368, 1_689_649_152)],
 )
 def test_perf_counter(model_name: str, active_params: int, flops_per_token: int):
-    # model = AutoModelForCausalLM.from_pretrained("Qwen/Qwen3-0.6B")
     config = AutoConfig.from_pretrained(model_name)
     model = AutoModelForCausalLM.from_config(config)
     perf_counter = PerfCounter(model, seq_len=1024, window_size=10)
