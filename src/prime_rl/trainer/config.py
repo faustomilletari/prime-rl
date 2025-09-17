d2c77d840a6b7f14504153548961171f1d72e094
Fausto Milletari <fmilletari@chanzuckerberg.com>
2025-09-17 00:12:42 -0700

    Revert "Merge remote-tracking branch 'upstream/main' into add-multinode-to-rl.py"
    This reverts commit c7840090baf863d25885522745af5fb132079518, reversing
    changes made to a2c8249a4a1e5c362ecd0fe01c0a57072879ca12.

--- src/prime_rl/trainer/config.py
+++ src/prime_rl/trainer/config.py
@@ -24,15 +24,6 @@ class ActivationCheckpointConfig(BaseModel):
     ] = 1
 
 
-class CompileConfig(BaseModel):
-    """Configures model compilation."""
-
-    fullgraph: Annotated[
-        bool,
-        Field(description="Whether to compile the transformer blocks with fullgraph."),
-    ] = False
-
-
 class ModelConfig(BaseConfig):
     """Configures the model for training."""
 
@@ -46,11 +37,11 @@ class ModelConfig(BaseConfig):
     attn: Annotated[AttnImplementation, Field(description="The attention implementation to use.")] = "flash_attention_2"
 
     compile: Annotated[
-        CompileConfig | None,
+        bool,
         Field(
             description="Whether to compile the model using `torch.compile`. Currently discouraged because it was found to destabilize training.",
         ),
-    ] = None
+    ] = False
 
     ac: Annotated[
         ActivationCheckpointConfig | None,
